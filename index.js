a66647b0a7925ab7a37ab50abddd09e396ea1e0b
raaynaldo <raaynaldo@hotmail.com>
2021-05-17 18:02:29 -0400

    feat: HTTP GET by id

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -13,12 +14,23 @@ app.get('/api/genres', (req, res) => {
   res.send(genres);
 });
 
+app.get('/api/genres/:id', (req, res) => {
+  const id = req.params.id;
+  const genre = genres.find((genre) => genre.id === parseInt(id));
+  if (!genre) {
+    return res.status(400).send('The genre with given ID was not found');
+  }
+
+  res.send(genre);
+});
+
 app.post('/api/genres', (req, res) => {
   // check if paramater is valid
   console.log(req.body);
   console.log(req.body.title);
-  if (!req.body.title) {
-    return res.status(400).send('The genre title is not existed');
+  const { error } = validateCourse(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
   }
 
   // save to genres
@@ -31,6 +43,14 @@ app.post('/api/genres', (req, res) => {
   // return the genre
   return res.send(genre);
 });
+
+const validateCourse = (course) => {
+  const schema = Joi.object({
+    title: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(course);
+};
 
 // PORT
 const port = process.env.PORT || 3000;
